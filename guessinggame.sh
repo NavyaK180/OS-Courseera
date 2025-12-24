60e150e89a2d7a7846865c6e4b43222ed5ceee17
NavyaK180 <2420030180@klh.edu.in>
2025-12-24 08:48:19 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Function to count files in current directory
+count_files() {
+    echo $(ls -1 | wc -l)
+}
+
+file_count=$(count_files)
+guess=0
+
+echo "Welcome to the Guessing Game!"
+echo "How many files are in the current directory?"
+
+while [[ $guess -ne $file_count ]]
+do
+    read -p "Enter your guess: " guess
+
+    if [[ $guess -lt $file_count ]]
+    then
+        echo "Your guess is too low. Try again."
+    elif [[ $guess -gt $file_count ]]
+    then
+        echo "Your guess is too high. Try again."
+    else
+        echo "🎉 Congratulations! You guessed the correct number of files."
+    fi
+done
